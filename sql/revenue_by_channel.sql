1113984f6171d0fe458497f70bbef29a5de352dc
terrynguyen19881215-del <terrynguyen19881215@gmail.com>
2026-03-09 22:53:48 +0700

    Add revenue by acquisition channel query

---
+++ sql/revenue_by_channel.sql
@@ -0,0 +1,10 @@
+SELECT
+  c.acquisition_channel,
+  COUNT(DISTINCT customer_id) AS customers,
+  SUM(amount_usd) AS revenue
+FROM payments p
+JOIN customers c
+  ON p.customer_id = c.customer_id
+WHERE status = 'paid'
+GROUP BY c.acquisition_channel
+ORDER BY revenue DESC;
