73f1a1d266db70eb589fce1d34de7202fe9f7a81
terrynguyen19881215-del <terrynguyen19881215@gmail.com>
2026-03-10 06:04:02 +0700

    Update top_customer_share.sql

--- sql/top_customer_share.sql
+++ sql/top_customer_share.sql
@@ -1,13 +1,27 @@
 -- Top Customer Revenue Contribution
+-- Top Revenue Contribution
 -- Calculates revenue share of top 5 value customers
 
+WITH revenue AS (
+	SELECT
+		customer_id,
+		SUM(amount_usd) AS customer_revenue
+	FROM payments
+	WHERE status = 'paid'
+	GROUP BY customer_id
+), total_revenue_table AS (
+	SELECT
+		customer_id,
+		customer_revenue,
+		SUM(r.customer_revenue) OVER () AS total_revenue
+	FROM revenue r
+)
+
 SELECT
-  c.customer_id,
-  SUM(p.amount_usd) AS total_revenue
-FROM payments p
-JOIN customers c
-  ON p.customer_id = c.customer_id
-WHERE p.status = 'paid'
-GROUP BY c.customer_id
-ORDER BY total_revenue DESC
+	customer_id,
+	customer_revenue,
+	ROUND(customer_revenue *100.0 /total_revenue, 1) AS revenue_share
+FROM total_revenue_table
+ORDER BY customer_revenue DESC
 LIMIT 5;
+
