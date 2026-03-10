85f5ac14dc2b42eae96dc735392d4ff1cd68c65f
Alice NG <terrynguyen19881215@gmail.com>
2026-03-11 00:42:17 +0700

    Update mrr_per_plan.sql

--- sql/mrr_per_plan.sql
+++ sql/mrr_per_plan.sql
@@ -4,7 +4,9 @@
 SELECT
     plan,
     COUNT(DISTINCT customer_id) AS customers,
-    SUM(mrr_usd) AS total_mrr
+    SUM(mrr_usd) AS total_mrr,
+    ROUND(AVG(mrr_usd), 2) AS avg_mrr_per_subscription,
+    ROUND(SUM(mrr_usd) * 100.0 / SUM(SUM(mrr_usd)) OVER (), 1) AS mrr_share_pct
 FROM subscriptions
 GROUP BY plan
 ORDER BY total_mrr DESC;
