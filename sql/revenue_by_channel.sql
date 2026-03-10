ffa5396c419cc9b6199db9a9260a388fd0da7ba0
Alice NG <terrynguyen19881215@gmail.com>
2026-03-11 00:41:45 +0700

    Update revenue_by_channel.sql

--- sql/revenue_by_channel.sql
+++ sql/revenue_by_channel.sql
@@ -4,7 +4,8 @@
 SELECT
   c.acquisition_channel,
   COUNT(DISTINCT customer_id) AS customers,
-  SUM(amount_usd) AS revenue
+  SUM(amount_usd) AS revenue,
+  ROUND(SUM(amount_usd) * 100.0 / SUM(SUM(amount_usd)) OVER (), 1) AS revenue_share_pct
 FROM payments p
 JOIN customers c
   ON p.customer_id = c.customer_id
